bdfa6e515757036a21e729b94892bc6e0c3d2434
Lothar Schulz <lothar.sch@gmx.de>
2019-10-09 16:24:58 +0200

    script service assumption added

--- web.sh
+++ web.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # simple test script to check the webservice endpoint deployed on minikube
+# this script assumes the web kubernetes service (https://github.com/lotharschulz/ktorjib/blob/master/k8s/service.yaml#L4)
+# is already deployed.
 ip=$(minikube ip)
 port=$(kubectl get svc web -o json | jq '.spec? | .ports? | .[] | .nodePort?')
 
