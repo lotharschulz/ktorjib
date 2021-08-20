ad3fc8089e4ca2ed1c702ca88670d15a7f2d9757
Lothar Schulz <lothar.sch@gmx.de>
2021-08-21 01:07:13 +0200

    chore import clean up

--- src/main/kotlin/info.ls.ktorjib/Main.kt
+++ src/main/kotlin/info.ls.ktorjib/Main.kt
@@ -3,8 +3,6 @@ package info.ls.ktorjib
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.features.DefaultHeaders
 import io.ktor.http.ContentType
 import io.ktor.response.respondText
 import io.ktor.routing.Routing
